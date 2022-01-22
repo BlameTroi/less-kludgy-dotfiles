c5dbf8f1e90bb724357aa2b8bcc7979c800d06a7
troi <blametroi@gmail.com>
2022-01-22 12:11:13 -0500

    Spacemacs, let's do this.
    I would prefer to completely roll my own, but my desire for Evil
    keybindings is greater. I think I've learned enough to find my way
    around.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,213 +1,73 @@
-;; troi's perpetual quest for a better init.el
+;;; init.el --- Spacemacs Initialization File -*- no-byte-compile: t -*-
 ;;
-;; stripping down and building up from scratch
+;; Copyright (c) 2012-2021 Sylvain Benner & Contributors
 ;;
-;; various articles and videos have influenced
-;; this effort, but as i've learned more and
-;; stopped using some of the usual starter kits
-;; i find the following useful:
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
 ;;
-;; http://github.com/susam/emacs4cl
-;; http://github.com/susam/emfy
-;; http://tuhdo.github.io
-;; http://emacswiki.org
-;; http://tess.oconnor.cx/config/.emacs
+;; This file is not part of GNU Emacs.
 ;;
-;; tess o'connor's config is particularly well
-;; laid out and documented.
-
-;; performance tweaks
-(add-hook 'focus-out-hook 'garbage-collect)
-(setq gc-cons-threshhold 1000000000)
-
-
-;; my code, overrides, and such
-(add-to-list 'load-path "~/.emacs.d/troi")
-(require 'troi-modal)
-
-
-;; encoding, utf-8 everywhere
-(setq-default buffer-file-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8)
-(set-language-environment 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-
-
-;; whitespace control
-(setq-default show-trailing-whitespace t)
-(setq-default indicate-empty-lines t)
-(setq-default indicate-buffer-boundaries 'left)
-(setq sentence-end-double-space nil)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-
-
-;; clean, quite, minimal, usable
-;;(menu-bar-mode 0)
-;;(when (display-graphic-p)
-;;  (tool-bar-mode 0)
-;;  (scroll-bar-mode 0))
-(column-number-mode)
-(setq inhibit-startup-screen t)
-(setq-default
- visible-bell t
- select-enable-clipboard t)
-(setq read-filename-completion-ignore-case t)
-(setq read-buffer-name-completion-ignore-case t)
-(xterm-mouse-mode)
-(fset 'yes-or-no-p 'y-or-n-p)         ; less typing
-
-;; theme
-;; currently using an old color theme until i figure
-;; out which custom theme i want and how to set it
-;; up.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; themes i find readable after some experimentation:
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 ;;
-;; alect-black, alect-black-alt, distinquished, grandshell,
-;; green-is-the-new-black, green-phosphor, green-screen,
-;; gruber-darker, hemisu-dark, ir-black, seti,
-;; base16-greenscreen, base16-irblack, base16-isotope,
-;; base16-seti, srcery, tron-legacy, deeper-blue,
-;; manoj-dark, wheatgrass
-;; (load-theme 'wheatgrass)
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-;; global default fonts
-(add-to-list 'default-frame-alist '(font . "Hack Nerd Font Mono-16"))
-(set-frame-font "Hack Nerd Font Mono-16" nil t)
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
+;; Avoid garbage collection during startup.
+;; see `SPC h . dotspacemacs-gc-cons' for more info
+(defconst emacs-start-time (current-time))
+(setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
+(load (concat (file-name-directory load-file-name)
+              "core/core-versions")
+      nil (not init-file-debug))
+(load (concat (file-name-directory load-file-name)
+              "core/core-load-paths")
+      nil (not init-file-debug))
+(load (concat spacemacs-core-directory "core-dumper")
+      nil (not init-file-debug))
 
-;; interactively do things
-(ido-mode 1)
-(ido-everywhere)
-(setq ido-enable-flex-matching t)
-(fido-mode)
+;; Remove compiled core files if they become stale or Emacs version has changed.
+(load (concat spacemacs-core-directory "core-compilation")
+      nil (not init-file-debug))
+(load spacemacs--last-emacs-version-file t (not init-file-debug))
+(when (or (not (string= spacemacs--last-emacs-version emacs-version))
+          (spacemacs//dir-contains-stale-byte-compiled-files-p
+           spacemacs-core-directory))
+  (spacemacs//remove-byte-compiled-files-in-dir spacemacs-core-directory))
+;; Update saved Emacs version.
+(unless (string= spacemacs--last-emacs-version emacs-version)
+  (spacemacs//update-last-emacs-version))
 
-
-;; keep auto saves and backups out of sight
-(make-directory "~/.tmp/emacs/auto-save/" t)
-(setq auto-save-file-name-transforms '((".*" "~/.tmp/emacs/auto-save/" t)))
-(setq backup-directory-alist '(("." . "~/.tmp/emacs/backup/")))
-
-
-;; do not move current file while creating backup
-(setq backup-by-copying t)
-
-
-;; paren matching when lisping
-(setq show-paren-delay 0)
-(show-paren-mode)
-
-
-;; customizations in a separate file
-;; *** this must come before any package-install ***
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file t)
-
-
-;; package management & melpa
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-(unless package-archive-contents
-  (package-refresh-contents))
-(package-initialize)
-
-
-;; make sure path is updated with shell path
-;; when launched from a gui shell it isn't.
-;;
-;; TODO: dedup path
-(defun troi/set-exec-path-from-shell-PATH ()
-  (interactive)
-  (let ((path-from-shell (replace-regexp-in-string
-                          "[ \t\n]*$" "" (shell-command-to-string
-                                          "$SHELL -c 'echo $PATH'"
-                                          ))))
-       (setenv "PATH" path-from-shell)
-       (setq exec-path (split-string path-from-shell path-separator))))
-(troi/set-exec-path-from-shell-PATH)
-
-
-;; install lisp development and other packages
-;; here if they are not already installed
-(dolist (package
-         '(
-           slime clhs rainbow-delimiters
-           which-key
-           go-mode forth-mode))
-  (unless (package-installed-p package)
-    (package-install package)))
-
-
-;; inferior lisp when sliming
-(setq inferior-lisp-program "sbcl")
-
-
-;; hooks for lisp with paredit and rainbow, there are
-;; many and the line by line explanation at susam/emacs4cl
-;; explains them well.
-
-
-;; rainbow delimiters
-(add-hook 'emacs-lisp-mode-hook
-          'rainbow-delimiters-mode)
-(add-hook 'ielm-mode-hook
-          'rainbow-delimiters-mode)
-(add-hook 'lisp-interaction-mode-hook
-          'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook
-          'rainbow-delimiters-mode)
-(add-hook 'slime-repl-mode-hook
-          'rainbow-delimiters-mode)
-
-
-;; and color them
-(require 'rainbow-delimiters)
-(set-face-foreground 'rainbow-delimiters-depth-1-face "#c66")  ; red
-(set-face-foreground 'rainbow-delimiters-depth-2-face "#6c6")  ; green
-(set-face-foreground 'rainbow-delimiters-depth-3-face "#69f")  ; blue
-(set-face-foreground 'rainbow-delimiters-depth-4-face "#cc6")  ; yellow
-(set-face-foreground 'rainbow-delimiters-depth-5-face "#6cc")  ; cyan
-(set-face-foreground 'rainbow-delimiters-depth-6-face "#c6c")  ; magenta
-(set-face-foreground 'rainbow-delimiters-depth-7-face "#ccc")  ; light gray
-(set-face-foreground 'rainbow-delimiters-depth-8-face "#999")  ; medium gray
-(set-face-foreground 'rainbow-delimiters-depth-9-face "#666")  ; dark gray
-
-
-;; help with keys
-(require 'which-key)
-(which-key-mode)
-
-
-;; org mode
-(require 'org)
-(setq org-log-done t)
-(setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
-
-
-;; cl, sbcl, and slime
-(load (expand-file-name "~/.quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-
-
-;; fortran
-(autoload 'f90-mode "f90" "Fortran 90 mode" t)
-(add-hook 'f90-mode-hook 'troi/f90-mode-hook)
-(defun troi/f90-mode-hook ()
-  (setq f90-font-lock-keywords f90-font-lock-keywords-3)
-  (abbrev-mode 1)                       ; turn on abbreviation mode
-  (turn-on-font-lock)                   ; syntax highlighting
-  (auto-fill-mode 0))                   ; turn off auto-filling
-
-
-;; key binds that don't belong anywhere else
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-
-;; more to come
+(if (not (version<= spacemacs-emacs-min-version emacs-version))
+    (error (concat "Your version of Emacs (%s) is too old. "
+                   "Spacemacs requires Emacs version %s or above.")
+           emacs-version spacemacs-emacs-min-version)
+  ;; Disable file-name-handlers for a speed boost during init
+  (let ((file-name-handler-alist nil))
+    (require 'core-spacemacs)
+    (spacemacs/dump-restore-load-path)
+    (configuration-layer/load-lock-file)
+    (spacemacs/init)
+    (configuration-layer/stable-elpa-init)
+    (configuration-layer/load)
+    (spacemacs-buffer/display-startup-note)
+    (spacemacs/setup-startup-hook)
+    (spacemacs/dump-eval-delayed-functions)
+    (when (and dotspacemacs-enable-server (not (spacemacs-is-dumping-p)))
+      (require 'server)
+      (when dotspacemacs-server-socket-dir
+        (setq server-socket-dir dotspacemacs-server-socket-dir))
+      (unless (server-running-p)
+        (message "Starting a server...")
+        (server-start)))))
